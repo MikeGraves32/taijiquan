fcae9a16d9489e3e64c2c65e15995a7d480fb566
MikeGraves32 <93281224+MikeGraves32@users.noreply.github.com>
2024-09-12 15:01:19 -0500

    Update Header.tsx

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,32 +2,45 @@
 import React from "react";
 import Link from "next/link";
 import Button from "@mui/material/Button";
+import { useRouter } from "next/navigation";
 import scss from "./Header.module.scss";
 import { Typography } from "@mui/material";
+import ThemeToggleButton from "@/app/components/ThemeToggleButton";
 import { UserDataType } from "@/app/hooks/useUserData";
-import ThemeToggler from "../ThemeToggler/ThemeToggler";
 
-export type HeaderProps = {
+interface HeaderProps {
   userData: UserDataType | null;
   ColorModeContext: React.Context<{ toggleColorMode: () => void }>;
-  currentMode: "light" | "dark";
+  currentMode: "light" | "dark"; // Prop for the current mode
   showLabel?: boolean;
-};
+}
 
-const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
-  const { userData, ColorModeContext, currentMode, showLabel } = props;
+const Header: React.FC<HeaderProps> = ({
+  userData,
+  ColorModeContext,
+  currentMode,
+  showLabel,
+}) => {
+  const router = useRouter(); // Initialize the useRouter hook
+  const handleSignOut = () => {
+    router.push("/logout");
+  };
+
+  const handleSignIn = () => {
+    router.push("/login");
+  };
+
   return (
     <header className={scss.header}>
       <nav className={scss.nav}>
         <ul className={scss.menu}>
           <li>
             <Button variant={"text"} href={"/"}>
-              <Typography variant="h6" style={{ textTransform: "initial" }}>
-                Taijiquan Courses
+              <Typography variant={"h6"} style={{ textTransform: "initial" }}>
+                CuriousCourses
               </Typography>
             </Button>
           </li>
-
           <li>
             <Link href="/">
               <Typography>Home</Typography>
@@ -48,48 +61,49 @@ const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
                   <Typography>Profile</Typography>
                 </Link>
               </li>
+              <li>
+                <Link href="/courses">
+                  <Typography>Courses</Typography>
+                </Link>
+              </li>
             </>
           )}
           <li style={{ marginLeft: "auto" }}>
-            <ThemeToggler
+            <ThemeToggleButton
               ColorModeContext={ColorModeContext}
               currentMode={currentMode}
-              showLabel={true}
-              userData={null}
+              showLabel={showLabel}
             />
           </li>
         </ul>
-
         {userData?.isLoggedIn ? (
           <Button
             className={scss.signOutBtn}
-            color="error"
-            href="/logout"
+            color={"error"}
+            onClick={handleSignOut}
             variant="contained"
           >
-            Sign out
+            Sign Out
           </Button>
         ) : (
           <>
-            <div className={scss.buttonGroup}>
-              <Button
-                className={scss.signOutBtn}
-                color="success"
-                href="/login"
-                variant="contained"
-                style={{ marginRight: "1rem" }}
-              >
-                Sign in
-              </Button>
-              <Button
-                className={scss.signOutBtn}
-                color="info"
-                href="/register"
-                variant="contained"
-              >
-                Register
-              </Button>
-            </div>
+            <Button
+              className={scss.signOutBtn}
+              color={"success"}
+              onClick={handleSignIn}
+              variant="contained"
+              style={{ marginRight: "1rem" }}
+            >
+              Sign In
+            </Button>
+            <Button
+              className={scss.signOutBtn}
+              color={"info"}
+              href={"/register"}
+              variant="contained"
+            >
+              Register
+            </Button>
           </>
         )}
       </nav>
